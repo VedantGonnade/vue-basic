477dcc8d9a919dcffe1de137d5370b800c342365
VedantGonnade <vedant_gonnade@LG-213.deltatre.it>
2023-09-19 19:50:13 +0200

    added logic how to use v-for directive in Vue

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const app = Vue.createApp({
   data() { 
     return {
       showBooks: true,
-      title: 'I am Ibmramovich',
-      Author: "Some Author",
-      age: 35,
-      x: 0,
-      y: 0
+      books: [
+        {
+          title: 'The Lord of the Rings',
+          author: 'Vedant Gonnade'
+        },
+        {
+          title: 'The Lord of the Ears',
+          author: 'Pooja Uke'
+        },
+        {
+          title: 'The Lord of the Mouth',
+          author: 'Bilal Baig'
+        }
+      ]
     }
   },
   methods: {
@@ -15,10 +24,6 @@ const app = Vue.createApp({
     },
     handleEvent(event, data) {
       console.log(event, event.type, data);
-    },
-    handleMouseMove(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
     }
   }
 });
