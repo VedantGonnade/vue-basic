8e6297035ad4f724bee5c6024dd80145ad18a510
VedantGonnade <vedant_gonnade@LG-213.deltatre.it>
2023-09-19 17:28:44 +0200

    added code for more mouse events like mouseover, mouseleave, doubleclick & mousemove

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const app = Vue.createApp({
       showBooks: true,
       title: 'I am Ibmramovich',
       Author: "Some Author",
-      age: 35
+      age: 35,
+      x: 0,
+      y: 0
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks =!this.showBooks;
+    },
+    handleEvent(event, data) {
+      console.log(event, event.type, data);
+    },
+    handleMouseMove(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
