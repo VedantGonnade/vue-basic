b01f44176133872bac1b6f8ef562e45fa06fae79
VedantGonnade <vedant_gonnade@LG-213.deltatre.it>
2023-09-22 16:42:52 +0200

    added code for props and click modifiers in Vue

--- app/modal-project/src/App.vue
+++ app/modal-project/src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <div v-if="showModel">
+    <Modal :header="header" :text="text" theme="sale" @closeModal="toggleModal"/>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'my First Vue app',
+      header: "Signup for a giveaway",
+      text: "grab your goodies!!",
+      showModel: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModel = !this.showModel
+    }
   }
 }
 </script>
@@ -22,5 +39,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
